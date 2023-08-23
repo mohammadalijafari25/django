8899c9011687cd5d182bc44d3b608bc4db417053
Mohammad Reza (Matin) Ahmadi <mra9776@gmail.com>
2023-08-23 13:43:01 +0330

    make some refactoring on code

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import requests
-bestMovies =[]
-for i in range(1,26):
+from requests import Session
+
+sess = Session()
+
+best_movies = []
+for i in range(26):
     params = {
         "page" : i
     }
     #print(params)
-    response = requests.get("http://moviesapi.ir/api/v1/movies",params = params)
-    for i in range(0,10):
-        print(response.json()["data"][i]['title'])
-        bestMovies.append(response.json()["data"][i]['title'])
+    response = sess.get("http://moviesapi.ir/api/v1/movies", params = params)
+    data = response.json()
+    for item in data['data']:
+        print(item['title'])
+        best_movies.append(item['title'])
 
-print(bestMovies)
-print((len(bestMovies)))
+print(best_movies)
+print((len(best_movies)))
